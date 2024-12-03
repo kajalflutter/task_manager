407b3c750882ecbcf9b254a6573f422364283caf
kajalflutter <156578934+kajalflutter@users.noreply.github.com>
2024-12-03 14:34:50 +0530

    Update db.js

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    password: '@7TRY#kk',
+    password: 'password',
     database: 'task_manager',
     waitForConnections: true,
     connectionLimit: 10,
